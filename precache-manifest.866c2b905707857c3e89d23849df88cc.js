e126006770875cefb64c1952713586eab8e02284
Sumit Kumar <sumitthrita@gmail.com>
2021-01-04 03:30:01 -0800

    Updates

---
+++ precache-manifest.866c2b905707857c3e89d23849df88cc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687f6b95de86a61b9b797da52d1f1cae",
+    "url": "/docar/index.html"
+  },
+  {
+    "revision": "b4213ab93abca7f313f4",
+    "url": "/docar/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "16b91faf36b8c45b4e81",
+    "url": "/docar/static/css/main.c21b85cb.chunk.css"
+  },
+  {
+    "revision": "b4213ab93abca7f313f4",
+    "url": "/docar/static/js/2.501a52d6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/docar/static/js/2.501a52d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b91faf36b8c45b4e81",
+    "url": "/docar/static/js/main.e7da4a16.chunk.js"
+  },
+  {
+    "revision": "8e3cdc1c2abea2dae294",
+    "url": "/docar/static/js/runtime-main.cd2bde47.js"
+  },
+  {
+    "revision": "7cd598ebd0647d784a0d59b5651aed49",
+    "url": "/docar/static/media/aboutUsImg.7cd598eb.jpg"
+  },
+  {
+    "revision": "286b1f1f731cd9dcb723f6255652fb0d",
+    "url": "/docar/static/media/logo.286b1f1f.jpg"
+  },
+  {
+    "revision": "053dc337021ca67eea5ae573734fab0a",
+    "url": "/docar/static/media/service1.053dc337.jpg"
+  },
+  {
+    "revision": "ca176a0ec05f07c6843424e8530586ea",
+    "url": "/docar/static/media/serviceAccident.ca176a0e.jpg"
+  },
+  {
+    "revision": "3a1ab0968e05118b8db61e3ac83ba1da",
+    "url": "/docar/static/media/serviceDiamond.3a1ab096.jpg"
+  },
+  {
+    "revision": "00cef1ea81bb255628707a9ae8ae9a56",
+    "url": "/docar/static/media/serviceGold.00cef1ea.jpg"
+  },
+  {
+    "revision": "28d8055e10a4b53298ee33e19d5af87b",
+    "url": "/docar/static/media/serviceOnRoad.28d8055e.jpg"
+  },
+  {
+    "revision": "2c511a638042432fdcd15794e3547453",
+    "url": "/docar/static/media/servicePlatinum.2c511a63.jpeg"
+  },
+  {
+    "revision": "8d3bbacf0bcedd742e97db156bddd1f1",
+    "url": "/docar/static/media/serviceSilver.8d3bbacf.jpg"
+  },
+  {
+    "revision": "ee2f652dc1d6584de859a9c1bf391d1e",
+    "url": "/docar/static/media/servicevector1.ee2f652d.jpg"
+  },
+  {
+    "revision": "8491564e4bd501c36805db24775a55a0",
+    "url": "/docar/static/media/servicevector2.8491564e.jpg"
+  },
+  {
+    "revision": "42452ab0987429f3ae6294bad1420c40",
+    "url": "/docar/static/media/toolsIcon1.42452ab0.jpg"
+  }
+]);
